b3a203cba36eec155893c26c4123af86708ae3b0
goolyuyi <goolyuyi@hotmail.com>
2021-05-11 16:06:25 +0800

    study init

--- 01_DirectoryBuildProps/SampleApp/Program.cs
+++ 01_DirectoryBuildProps/SampleApp/Program.cs
@@ -10,7 +10,7 @@ namespace SampleApp
             var assembly = typeof(Program).Assembly;
             Console.WriteLine($"This is sample app version {assembly.GetName().Version} by {assembly.GetCustomAttribute<AssemblyCompanyAttribute>().Company}.");
             
-            var unused = "something";
+            // var unused = "something";
         }
     }
 }
